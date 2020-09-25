29e58509fdaf947b014b506df11f1b908217776a
tomaszml <56832614+tomaszml@users.noreply.github.com>
2020-09-26 01:18:27 +0200

    Footer

---
+++ resources/views/layouts/components/footer.blade.php
@@ -0,0 +1,19 @@
+<footer>
+    <div>
+        <section class="brand">
+            <h1>PastaShare</h1>
+            <span>E-mail kontaktowy: <b>pastashare@cttm.pl</b></span>
+        </section>
+        <section class="links">
+            <h4>Uproszczona nawigacja</h4>
+            <ul>
+                <li>
+                    <a href="{{ url('/') }}">Główna</a>
+                </li>
+                <li>
+                    <a href="{{ url('/') }}">Popularne</a>
+                </li>
+            </ul>
+        </section>
+    </div>
+</footer>
